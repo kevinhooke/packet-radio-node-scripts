1b9137c3271a86b86779f331195aae4fb1554121
Kevin Hooke <kevin@kevinhooke.com>
2021-11-17 19:04:58 -0800

    updated rss arrl example with line num output

--- rss-arrl-example.sh
+++ rss-arrl-example.sh
@@ -6,8 +6,11 @@ echo "http://www.arrl.org/news"
 echo ""
 IFS=$'\n' lines=`curl -s -X GET https://[your-api-gw-id-here].execute-api.us-west-1.amazonaws.com/dev/parse?rss=http://www.arrl.org/arrl.rss | jq '.headlines[]'`
 
+lineNum=0
 #write each line individually to output
 for line in $lines;
   do
-    echo "$line"
+    echo "[$lineNum] $line"
+    ((lineNum=lineNum+1))
   done
+echo "To display article content, enter R and article number when prompted"
